d8aba55304e1654723c43501d67db1064603b75c
alrahmads <arhma62@gmail.com>
2024-09-17 23:39:07 +0700

    merge

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/rp_flask_board/venv"
+set -gx VIRTUAL_ENV "/root/Simple-Flask-Web-Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
